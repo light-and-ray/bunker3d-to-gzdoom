2d009c4f4643556fa18e58713816f84f6d6120cb
Andray <light.and.ray@gmail.com>
2025-03-03 12:51:28 +0400

    target generatePalette to `static` directory

--- bin/generatePalette.sh
+++ bin/generatePalette.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 cd "$(dirname "$0")"
-palettePath="../result.d/playpal.lmp"
-colormapPath="../result.d/colormap.lmp"
+palettePath="../static/playpal.lmp"
+colormapPath="../static/colormap.lmp"
 find ../result.d/ -iname "*.png" | xargs python3 _generatePalette/optimal_palette.py "$palettePath"
 python3 _generatePalette/colormap.py "$palettePath" > "$colormapPath"
