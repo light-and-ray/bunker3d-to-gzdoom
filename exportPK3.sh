5ea39a31305a7be15b26409027a7ef14f7006b6b
Andray <light.and.ray@gmail.com>
2025-03-05 08:38:27 +0400

    update `launch.sh` and `exportPK3.sh`

--- exportPK3.sh
+++ exportPK3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 cd result.d
 rm -f ../bunker3d_alpha.pk3
 zip -rq ../bunker3d_alpha.pk3 .
--- launch.sh
+++ launch.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
 gzdoom -iwad DOOM.WAD -file result.d "$@"
